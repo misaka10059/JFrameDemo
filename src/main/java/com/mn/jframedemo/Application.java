61832ddc85c2471be83e29fcc39b9fa8ad2e5028
MisakaNetwork <2269656714g@gmail.com>
2020-07-01 11:15:21 +0800

    初始化项目结构

---
+++ src/main/java/com/mn/jframedemo/Application.java
@@ -0,0 +1,16 @@
+package com.mn.jframedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AUTHOR MisakaNetwork
+ * DATE 2020/7/1 11:04
+ * DESC
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
